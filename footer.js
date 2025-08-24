4e175db55efe2983aef1d0c31ce2c0bad83010f1
Galdrakarlinn <veiga@rok.is>
2025-08-24 22:55:05 +0000

    Uppfærsla: nýjar síður, style.css, under construction banner

---
+++ footer.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("footer.html")
+      .then(response => {
+        if (!response.ok) throw new Error("HTTP error " + response.status);
+        return response.text();
+      })
+      .then(data => {
+        document.getElementById("footer-placeholder").innerHTML = data;
+      })
+      .catch(error => console.error("Error loading footer:", error));
+  });
+  
---
+++ nav.js
@@ -0,0 +1,10 @@
+// nav.js
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("nav.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("nav-placeholder").innerHTML = data;
+      })
+      .catch(error => console.error("Error loading navigation:", error));
+  });
+  
